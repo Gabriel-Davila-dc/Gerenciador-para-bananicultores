14aa9efefbe8ca6b1a9cbeb0e5a138f45b649116
gabriel19nh-web <gabriel19nh@gmail.com>
2026-04-01 21:44:20 -0300

    sobre atualizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { Gerenciador } from './pages/gerenciador/gerenciador';
 import { Sobre } from './pages/sobre/sobre';
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
-
 export const routes: Routes = [
   {
     path: '',
